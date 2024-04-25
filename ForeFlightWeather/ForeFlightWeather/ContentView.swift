297f9bd9259c957f7bcea6043a57e506dbc7f30c
Christopher Williams <chrsw49@gmail.com>
2024-04-25 08:57:03 -0600

    initial commit with PDF specs and Xcode empty project

---
+++ ForeFlightWeather/ForeFlightWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForeFlightWeather
+//
+//  Created by Christopher Williams on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForeFlightWeather/ForeFlightWeather/ForeFlightWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeFlightWeatherApp.swift
+//  ForeFlightWeather
+//
+//  Created by Christopher Williams on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ForeFlightWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
