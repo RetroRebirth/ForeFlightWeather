87e662f167b496ec0b0811d49742b9debdaca0ac
Christopher Williams <chrsw49@gmail.com>
2024-04-25 09:15:45 -0600

    Added basic TextField from tutorial

--- ForeFlightWeather/ForeFlightWeather/ContentView.swift
+++ ForeFlightWeather/ForeFlightWeather/ContentView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var airport: String = "KPWM"
+    @FocusState private var airportFieldIsFocused: Bool
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            TextField(
+                "Airport",
+                text: $airport
+            )
+            .focused($airportFieldIsFocused)
+            .onSubmit {
+//                validate(name: airport)
+            }
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+
+            Text(airport)
+                .foregroundColor(airportFieldIsFocused ? .red : .blue)
         }
         .padding()
     }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ForeFlightWeather
 An iOS app that fetches and displays the weather report for a given airport from ForeFlight's QA server, this time with up to date SDKs!
+
+TODO: insert photo/video
+
+## How long was spent on the project
+- Started around 9am April 25th 2024.
+
+## Any notable design decisions you wish to highlight
+
+## Any references consulted and/or third party libraries used
+### Swift UI tutorials
+- TextField: https://developer.apple.com/documentation/swiftui/textfield
+
+## Known issues
+
+## Any other notes that will help us understand your project
+- This project is my second attempt using a 2024 Macbook Pro with iOS 17.4, Xcode 15.3, macOS 14.4, and Swift 5.10.
+
